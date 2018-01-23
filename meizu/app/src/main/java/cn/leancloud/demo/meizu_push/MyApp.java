6a2ed33f5f961cd886887e6c33212ca4e25b4802
jwfing <jwfing@gmail.com>
2018-01-23 21:28:03 +0800

    add local jar

--- meizu/app/src/main/java/cn/leancloud/demo/meizu_push/MyApp.java
+++ meizu/app/src/main/java/cn/leancloud/demo/meizu_push/MyApp.java
@@ -1,8 +1,10 @@
 package cn.leancloud.demo.meizu_push;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.avos.avoscloud.*;
+import com.alibaba.fastjson.JSON;
 
 import com.meizu.cloud.pushsdk.util.MzSystemUtils;
 
@@ -20,10 +22,10 @@ public class MyApp extends Application {
   @Override
   public void onCreate() {
     super.onCreate();
-    AVOSCloud.setDebugLogEnabled(true);
     AVOSCloud.initialize(this,LC_APP_ID,LC_APP_KEY);
-    if(MzSystemUtils.isBrandMeizu(this)){
-      AVMixpushManager.registerFlymePush(this, MEIZU_APP, MEIZU_KEY);
-    }
+    AVOSCloud.setDebugLogEnabled(true);
+    AVMixpushManager.registerFlymePush(this, MEIZU_APP, MEIZU_KEY);
+
+    PushService.setDefaultPushCallback(this, MainActivity.class);
   }
 }
