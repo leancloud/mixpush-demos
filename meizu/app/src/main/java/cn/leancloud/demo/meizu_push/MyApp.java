890f61d131e394dbc6634e7a43d11d203229fa89
jwfing <jwfing@gmail.com>
2018-01-31 13:49:37 +0800

    update avoscloud sdk version

--- meizu/app/src/main/java/cn/leancloud/demo/meizu_push/MyApp.java
+++ meizu/app/src/main/java/cn/leancloud/demo/meizu_push/MyApp.java
@@ -1,12 +1,8 @@
 package cn.leancloud.demo.meizu_push;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.avos.avoscloud.*;
-import com.alibaba.fastjson.JSON;
-
-import com.meizu.cloud.pushsdk.util.MzSystemUtils;
 
 /**
  * Created by fengjunwen on 2018/1/22.
@@ -22,9 +18,12 @@ public class MyApp extends Application {
   @Override
   public void onCreate() {
     super.onCreate();
+
     AVOSCloud.initialize(this,LC_APP_ID,LC_APP_KEY);
     AVOSCloud.setDebugLogEnabled(true);
-    AVMixpushManager.registerFlymePush(this, MEIZU_APP, MEIZU_KEY);
+
+    boolean registResult = AVMixPushManager.registerFlymePush(this, MEIZU_APP, MEIZU_KEY);
+    System.out.println("register result;" + registResult);
 
     PushService.setDefaultPushCallback(this, MainActivity.class);
   }
