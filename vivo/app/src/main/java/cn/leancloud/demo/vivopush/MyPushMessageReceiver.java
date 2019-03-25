f9cb5fbbc25e3f397f1e411b0c335ccac7856d47
jwfing <jwfing@gmail.com>
2019-03-25 11:43:11 +0800

    chore

--- vivo/app/src/main/java/cn/leancloud/demo/vivopush/MyPushMessageReceiver.java
+++ vivo/app/src/main/java/cn/leancloud/demo/vivopush/MyPushMessageReceiver.java
@@ -3,13 +3,14 @@ package cn.leancloud.demo.vivopush;
 import android.content.Context;
 
 import com.avos.avoscloud.AVVIVOPushMessageReceiver;
+import com.vivo.push.model.UPSNotificationMessage;
 
 import java.util.logging.Logger;
 
 public class MyPushMessageReceiver extends AVVIVOPushMessageReceiver {
   private static final Logger logger = Logger.getLogger(MyPushMessageReceiver.class.getSimpleName());
 
-  public void onNotificationMessageClicked(Context var1, com.vivo.push.model.UPSNotificationMessage var2) {
+  public void onNotificationMessageClicked(Context var1, UPSNotificationMessage var2) {
     ;
   }
 }
