065cc665ce5b9a3b07892f00bbff45ba0da0746e
jwfing <jwfing@gmail.com>
2019-09-18 11:40:03 +0800

    update fcm dependency to java unified sdk

--- fcm/app/src/main/java/cn/leancloud/fcmdemo/MyApp.java
+++ fcm/app/src/main/java/cn/leancloud/fcmdemo/MyApp.java
@@ -2,8 +2,9 @@ package cn.leancloud.fcmdemo;
 
 import android.app.Application;
 
-import com.avos.avoscloud.AVOSCloud;
-import com.avos.avoscloud.PushService;
+import cn.leancloud.AVLogger;
+import cn.leancloud.AVOSCloud;
+import cn.leancloud.push.PushService;
 
 /**
  * Created by fengjunwen on 2018/5/28.
@@ -13,8 +14,8 @@ public class MyApp extends Application {
   @Override
   public void onCreate() {
     super.onCreate();
-    AVOSCloud.setDebugLogEnabled(true);
-    AVOSCloud.useAVCloudUS();
+    AVOSCloud.setLogLevel(AVLogger.Level.DEBUG);
+    AVOSCloud.setRegion(cn.leancloud.core.AVOSCloud.REGION.NorthAmerica);
     AVOSCloud.initialize(this, "glvame9g0qlj3a4o29j5xdzzrypxvvb30jt4vnvm66klph4r",
         "n79rw9ja3eo8n8au838t7pqur5mw88pnnep6ahlr99iq661a");
     PushService.setDefaultPushCallback(this, MainActivity.class);
