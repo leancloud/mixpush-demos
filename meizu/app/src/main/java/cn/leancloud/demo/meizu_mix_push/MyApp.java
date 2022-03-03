5f0404cd2fe70b309d3090750c17a154615d3dbf
xsui <284898541@qq.com>
2022-03-04 00:10:44 +0800

    add lc and tds apk files

--- meizu/app/src/main/java/cn/leancloud/demo/meizu_mix_push/MyApp.java
+++ meizu/app/src/main/java/cn/leancloud/demo/meizu_mix_push/MyApp.java
@@ -9,6 +9,12 @@ import cn.leancloud.LeanCloud;
 public class MyApp extends Application {
     private static final String LC_APP_ID = "Gvv2k8PugDTmYOCfuK8tiWd8-gzGzoHsz";
     private static final String LC_APP_KEY = "dpwAo94n81jPsHVxaWwdxJVu";
+    private static final String LC_APP_URL = "https://gvv2k8pu.lc-cn-n1-shared.com";
+
+
+    private static final String TDS_APPID = "HoiGvMeacbPWnv12MK";
+    private static final String TDS_APPKEY = "FesqQUmlhjMWt6uNrKaV6QPtYgBYZMP9QFmTUk54";
+    private static final String TDS_ServerURL = "https://hoigvmea.cloud.tds1.tapapis.cn";
 
     private static final String MEIZU_APP = "146951";
     private static final String MEIZU_KEY = "304015a4ef864d05a56e863b7582eeb8";
@@ -17,7 +23,8 @@ public class MyApp extends Application {
     public void onCreate() {
         super.onCreate();
         LeanCloud.setLogLevel(LCLogger.Level.DEBUG);
-        LeanCloud.initialize(this,LC_APP_ID,LC_APP_KEY, "https://gvv2k8pu.lc-cn-n1-shared.com");
+        LeanCloud.initialize(this,LC_APP_ID,LC_APP_KEY, LC_APP_URL);
+//        LeanCloud.initialize(this, TDS_APPID,TDS_APPKEY,TDS_ServerURL);
 
         boolean registResult = LCMixPushManager.registerFlymePush(this, MEIZU_APP, MEIZU_KEY);
         System.out.println("register result;" + registResult);
--- oppo/app/src/main/java/cn/leancloud/demo/oppopush/MyApplication.java
+++ oppo/app/src/main/java/cn/leancloud/demo/oppopush/MyApplication.java
@@ -9,6 +9,12 @@ import cn.leancloud.LeanCloud;
 public class MyApplication extends Application {
   private static final String LeanCloud_APPID = "Gvv2k8PugDTmYOCfuK8tiWd8-gzGzoHsz"; // your leancloud app id
   private static final String LeanCloud_APPKEY = "dpwAo94n81jPsHVxaWwdxJVu"; // your leancloud app key.
+  private static final String LeanCloud_ServerURL = "https://gvv2k8pu.lc-cn-n1-shared.com"; // your leancloud app key.
+
+  //tds
+  private static final String TDS_APPID = "HoiGvMeacbPWnv12MK";
+  private static final String TDS_APPKEY = "FesqQUmlhjMWt6uNrKaV6QPtYgBYZMP9QFmTUk54";
+  private static final String TDS_ServerURL = "https://hoigvmea.cloud.tds1.tapapis.cn";
 
   public static final String OPPO_APPKEY = "87b1d6c3a426496aab9931d94ee890c2";
   public static final String OPPO_APPSECRET = "8a443674d9ae45b49cfacfb288b50105";//""your oppo app secret";
@@ -20,7 +26,9 @@ public class MyApplication extends Application {
     // 开启调试日志
     LeanCloud.setLogLevel(LCLogger.Level.DEBUG);
     // 初始化参数依次为 this, AppId, AppKey
-    LeanCloud.initialize(this, LeanCloud_APPID, LeanCloud_APPKEY, "https://gvv2k8pu.lc-cn-n1-shared.com");
+        LeanCloud.initialize(this, TDS_APPID,TDS_APPKEY,TDS_ServerURL);
+//    LeanCloud.initialize(this, LeanCloud_APPID, LeanCloud_APPKEY,LeanCloud_ServerURL);
+
     LCMixPushManager.registerOppoPush(this, OPPO_APPKEY, OPPO_APPSECRET, new MyPushAdapter());
   }
 }
