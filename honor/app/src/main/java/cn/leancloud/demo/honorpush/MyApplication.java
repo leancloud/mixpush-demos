339abf4169b0c5f5445ffc7ddf69585493322ee5
jwfing <jwfing@gmail.com>
2023-02-10 12:12:48 +0800

    add honor sample

---
+++ honor/app/src/main/java/cn/leancloud/demo/honorpush/MyApplication.java
@@ -0,0 +1,21 @@
+package cn.leancloud.demo.honorpush;
+
+import android.app.Application;
+
+import cn.leancloud.LeanCloud;
+import cn.leancloud.honor.LCMixPushManager;
+import cn.leancloud.push.PushService;
+
+public class MyApplication extends Application {
+    private static final String LC_APP_ID = "Gvv2k8PugDTmYOCfuK8tiWd8-gzGzoHsz";
+    private static final String LC_APP_KEY = "dpwAo94n81jPsHVxaWwdxJVu";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        LeanCloud.initialize(this, LC_APP_ID, LC_APP_KEY, "https://gvv2k8pu.lc-cn-n1-shared.com");
+        LCMixPushManager.registerHonorPush(this);
+        PushService.setDefaultPushCallback(this, MainActivity.class);
+    }
+}
